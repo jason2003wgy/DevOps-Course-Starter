bcfe672ed539c8d840c53b94be8ef3469adb6282
Jason Wang <xiayan.wang@gmail.com>
2021-05-27 22:51:40 +0100

    now show items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from todo_app.flask_config import Config
+from todo_app.data.session_items import add_item, get_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -7,7 +8,8 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    lst_item = get_items()
+    return render_template('index.html', lst_item=lst_item)
 
 
 if __name__ == '__main__':
