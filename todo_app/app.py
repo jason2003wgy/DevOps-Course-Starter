6cb7b56332bb17a40c920306d0a2634f67c4774f
Jason Wang <xiayan.wang@gmail.com>
2021-05-27 22:05:06 +0100

    My first commit!

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, render_template
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
